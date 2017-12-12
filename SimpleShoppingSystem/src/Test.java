0dfd1dc94b1f142291c00101cdffe43ac9bd308c
iversongf <33052586+iversongf@users.noreply.github.com>
2017-12-12 15:55:51 +0800

    Create Test.java
    Test request

---
+++ SimpleShoppingSystem/src/Test.java
@@ -0,0 +1,46 @@
+package SimpleShoppingSystem;
+
+public class Text {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Usergoods go=new Usergoods();
+			/*go.setId("1");
+			go.setName1("巧克力");
+			go.setPhoto("chocolate.jpg");
+			go.setPrice((float)6.3);
+			go.savegoods();
+			
+			go.setId("2");
+			go.setName1("毛巾");
+			go.setPhoto("maojin.jpg");
+			go.setPrice((float)2.1);
+			go.savegoods();
+			
+			go.setId("3");
+			go.setName1("卷纸");
+			go.setPhoto("milck.jpg");
+			go.setPrice((float)9.3);
+			go.savegoods();
+			
+			go.setId("4");
+			go.setName1("牙刷");
+			go.setPhoto("teethbrush.jpg");
+			go.setPrice((float)16.8);
+			go.savegoods();
+			
+			go.setId("5");
+			go.setName1("洗发水");
+			go.setPhoto("xifashuiu.jpg");
+			go.setPrice((float)24.6);
+			go.savegoods();*/
+			go.getgoodsid("1");
+			System.out.print(go.getPhoto());
+			System.out.println();
+			go.getPrice();
+			System.out.println(go.getPrice());
+			
+
+	}
+
+}
